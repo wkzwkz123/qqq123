9b15d0cf1ba277a5086a6306805c60c2b74954f6
dell <1091838518@qq.com>
2017-09-12 08:42:18 +0800

    第六次提交

--- day01_homework/src/main/java/com/jiyun/day01_homework/fragment/BlankFragment2.java
+++ day01_homework/src/main/java/com/jiyun/day01_homework/fragment/BlankFragment2.java
@@ -7,9 +7,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.jiyun.day01_homework.Bean.Bean;
+import com.jiyun.day01_homework.Main2Activity;
 import com.jiyun.day01_homework.R;
 import com.youth.banner.Banner;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -19,6 +24,7 @@ public class BlankFragment2 extends Fragment {
 
     private Banner banner;
     private TextView tv;
+    private Main2Activity activity;
 
     public BlankFragment2() {
         // Required empty public constructor
@@ -30,7 +36,18 @@ public class BlankFragment2 extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_blank_fragment2, container, false);
         initView(view);
+        activity = (Main2Activity) getActivity();
+        initData();
         return view;
+    }
+
+    private void initData() {
+        activity.blankFragment.setOnClicker(new BlankFragment.OnClicker() {
+            @Override
+            public void sendString(ArrayList<Bean.DataBean> mList, int i) {
+                Toast.makeText(activity, ""+i, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     private void initView(View view) {
